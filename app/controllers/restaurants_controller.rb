94120406981a2720337a29501a724e29aa0f0478
Jennifer Parks <jp00213@my.westga.edu>
2023-04-19 18:21:44 -0400

    Now requires authentication for everything except index and show. Index displays different columns and links according to login status

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, :except => [:index, :show]
 
   # GET /restaurants or /restaurants.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :restaurants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: "home#index"
+  root to: "restaurants#index"
   
   put "/increaseUpvote", :to=>"restaurants#increaseUpvote"
   put "/increaseDownvote", :to=>"restaurants#increaseDownvote"
